55a898f8d8a7958c52053af4af48d18337886018
KangDongsung <kang1270ds@gmail.com>
2024-12-27 16:27:22 +0900

    fix: adjusting the screen

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,51 +26,6 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Nav/>
-        {/* <header className="mobile-view"> */}
-{/* 
-          <div className="nav-var">
-            <div className="logo" href="/"><a>DS_HOME</a></div>
-            <div className="nav-menu">
-              <ul>
-                <li>
-                  <a className="item" href="/my_portfolio/">Home</a>
-                </li>
-                <li>
-                  <a className="item" href="About">About</a>
-                </li>
-                <li>
-                  <a className="item" href="Project">Project</a>
-                </li>
-              </ul>
-            </div>
-            <div className="mobile-menu menu-open">
-              <span/>
-              <span/>
-              <span/>
-            </div>
-          </div> */}
-
-        {/* </header> */}
-
-        {/* 모바일 메뉴 */}
-        {/* <div className="mobile-frame menu-close" > */}
-        {/* <div className="mobile-frame">
-          <div className="mobile-list-frame">
-            <ul className="mobile-list">
-              <li>  
-                <a href="/my_portfolio/">Home</a>
-              </li>
-              <li>
-                <a href="About">About</a>
-              </li>
-              <li>
-                <a href="Project">Project</a>
-              </li>
-            </ul>
-          </div>
-          <div className="mobile-backgraund"/>
-        </div> */}
-        {/* 모바일 메뉴 */}
         
         {children}
         
